afa10db1b1ca8b1de55807a5a75e204eee455831
蔡皓全 <a22646185@gmail.com>
2021-03-16 11:54:19 +0800

    5

--- all.js
+++ all.js
@@ -1,6 +1,29 @@
-$(document).ready(function(){
-    $('.phoneMenu').on('click',function(e){
+
+
+$(document).ready(function () {
+    $('.phoneMenu').click(function (e) {
         e.preventDefault();
         $('body').toggleClass('menu-show');
-    })
+    });
+     
+    $('.scrollTop').click(function(e){
+        e.preventDefault();
+        let target = $(this).attr('href');
+        let targetPos = $(target).offset().top;
+        $('html,body').animate({scrollTop:targetPos},1000);
+    });
+
+    $(window).scroll(function(){
+        let scrollPos = $(window).scrollTop();
+        $('.scrollTop').each(function(){
+            let target = $(this).attr('href');
+            let targetPos = $(target).offset().top;
+            let targetHei = $(target).outerHeight();
+            console.log(target,targetPos,targetHei)
+            if(targetPos <= scrollPos && (targetPos +targetHei)>scrollPos){
+                console.log('ya')
+            }
+        });
+    });
+
 })
