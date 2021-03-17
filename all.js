a2c4010676bd755d3cc1c341e36995a8e3987eb4
蔡皓全 <a22646185@gmail.com>
2021-03-17 17:43:42 +0800

    6

--- all.js
+++ all.js
@@ -5,25 +5,54 @@ $(document).ready(function () {
         e.preventDefault();
         $('body').toggleClass('menu-show');
     });
-     
-    $('.scrollTop').click(function(e){
+
+    /*
+        $('.pcMenu li').click(function (e) {
+            e.preventDefault();
+            $('body').toggleClass('menu-show');
+        })
+    */
+    $('.scrollTop').click(function (e) {
         e.preventDefault();
         let target = $(this).attr('href');
         let targetPos = $(target).offset().top;
-        $('html,body').animate({scrollTop:targetPos},1000);
+        $('html,body').animate({ scrollTop: targetPos }, 1000);
     });
 
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         let scrollPos = $(window).scrollTop();
-        $('.scrollTop').each(function(){
+        let windowHei = $(window).height();
+
+        $('.scrollTop').each(function () {
             let target = $(this).attr('href');
             let targetPos = $(target).offset().top;
             let targetHei = $(target).outerHeight();
-            console.log(target,targetPos,targetHei)
-            if(targetPos <= scrollPos && (targetPos +targetHei)>scrollPos){
-                console.log('ya')
+            if (targetPos - 1 <= scrollPos && (targetPos + targetHei) > scrollPos) {
+                $('.scrollTop').removeClass('active');
+                $(this).addClass('active')
+            } else {
+                $(this).removeClass('active');
+            }
+        });
+
+        $('.animate').each(function () {
+            let thisPos = $(this).offset().top;
+            if ((windowHei / 1.3 + scrollPos) >= thisPos) {
+                $(this).addClass('fadeIn');
+            }
+        });
+        $('.animate2').each(function () {
+            let thisPos = $(this).offset().top;
+            if ((windowHei / 1.3 + scrollPos) >= thisPos) {
+                $(this).addClass('fadeLeft');
+            }
+        });
+        $('.animate3').each(function () {
+            let thisPos = $(this).offset().top;
+            if ((windowHei / 1.3 + scrollPos) >= thisPos) {
+                $(this).addClass('fadeRight');
             }
         });
     });
 
-})
+});
